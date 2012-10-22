263462756408ace0dc420892ab7af9f41692958b
Lars Pensjo <lars.pensjo@gmail.com>
2012-10-22 08:06:48 +0200

    Change nanme of distro.

--- bin/makedistro.sh
+++ bin/makedistro.sh
@@ -2,5 +2,5 @@
 cp ../dumpfile.sql .
 strip server
 strip shell
-tar cvfz distro-linux-`date +%F`.gz server shell dumpfile.sql readme.md config.ini
+tar cvfz distro-linux64-`date +%F`.gz server shell dumpfile.sql readme.md config.ini
 rm dumpfile.sql
